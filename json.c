b70813e1b1b7459a38ee7327f9b0c32501cdf56b
dominicattal <attalientidominic@gmail.com>
2025-05-21 04:04:34 -0400

    changed test casees

--- json.c
+++ json.c
@@ -1,4 +1,5 @@
 #include "json.h"
+#include <dirent.h>
 #include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -34,17 +35,17 @@ typedef struct JsonValue {
     };
 } JsonValue;
 
-static JsonValue* parse_value(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonValue* parse_value_object(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonValue* parse_value_array(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonValue* parse_value_number(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonValue* parse_value_string(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonValue* parse_value_true(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonValue* parse_value_false(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonValue* parse_value_null(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonObject* parse_object(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonMember* parse_member(const char* path, FILE* file, int* line_num, int* error_count);
-static JsonMember** parse_members(const char* path, FILE* file, int* line_num, int* error_count);
+static JsonValue* parse_value(FILE* file, int* line_num);
+static JsonValue* parse_value_object(FILE* file, int* line_num);
+static JsonValue* parse_value_array(FILE* file, int* line_num);
+static JsonValue* parse_value_number(FILE* file, int* line_num);
+static JsonValue* parse_value_string(FILE* file, int* line_num);
+static JsonValue* parse_value_true(FILE* file, int* line_num);
+static JsonValue* parse_value_false(FILE* file, int* line_num);
+static JsonValue* parse_value_null(FILE* file, int* line_num);
+static JsonObject* parse_object(FILE* file, int* line_num);
+static JsonMember* parse_member(FILE* file, int* line_num);
+static JsonMember** parse_members(FILE* file, int* line_num);
 
 static void json_members_destroy(JsonMember** members)
 {
@@ -70,9 +71,9 @@ static JsonMember** push_member(JsonMember** members, JsonMember* member, int* l
     return members;
 }
 
-static void print_error(const char* path, const int* line_num, const char* message)
+static void print_error(const int* line_num, const char* message)
 {
-    printf("Error parsing json file %s [%d]: %s\n", path, *line_num, message);
+    printf("[%d]: %s\n", *line_num, message);
 }
 
 static char getch(FILE* file, int* line_num)
@@ -116,14 +117,14 @@ static long get_next_char_pos(FILE* file, int* line_num, char ch)
 }
 
 // gets next string surrounded by quotes, returns NULL if not valid
-static const char* get_next_string(const char* path, FILE* file, int* line_num)
+static const char* get_next_string(FILE* file, int* line_num)
 {
     char c;
     long start_pos, end_pos;
 
     c = get_next_nonspace(file, line_num);
     if (c != '"') {
-        print_error(path, line_num, "Missing quotes");
+        print_error(line_num, "Missing quotes");
         return NULL;
     }
 
@@ -132,7 +133,7 @@ static const char* get_next_string(const char* path, FILE* file, int* line_num)
     end_pos = get_next_char_pos(file, line_num, '"');
     ASSERT(end_pos != -1);
     if (end_pos == -2) {
-        print_error(path, line_num, "Expected closing quotes");
+        print_error(line_num, "Expected closing quotes");
         return NULL;
     }
 
@@ -144,38 +145,35 @@ static const char* get_next_string(const char* path, FILE* file, int* line_num)
     return string;
 }
 
-static JsonValue* parse_value_object(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonValue* parse_value_object(FILE* file, int* line_num)
 {
-    UNUSED(path);
     UNUSED(file);
     UNUSED(line_num);
-    UNUSED(error_count);
     return NULL;
 }
 
-static JsonValue* parse_value_array(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonValue* parse_value_array(FILE* file, int* line_num)
 {
-    UNUSED(path);
     UNUSED(file);
     UNUSED(line_num);
-    UNUSED(error_count);
     return NULL;
 }
 
-static JsonValue* parse_value_number(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonValue* parse_value_number(FILE* file, int* line_num)
 {
-    UNUSED(path);
     UNUSED(file);
     UNUSED(line_num);
-    UNUSED(error_count);
     return NULL;
 }
 
-static JsonValue* parse_value_string(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonValue* parse_value_string(FILE* file, int* line_num)
 {
-    UNUSED(error_count);
 
-    const char* string = get_next_string(path, file, line_num);
+    const char* string = get_next_string(file, line_num);
+    if (string == NULL) {
+        print_error(line_num, "Error parsing value string");
+        return NULL;
+    }
 
     JsonValue* value = malloc(sizeof(JsonValue));
     value->type = JTYPE_STRING;
@@ -184,74 +182,68 @@ static JsonValue* parse_value_string(const char* path, FILE* file, int* line_num
     return value;
 }
 
-static JsonValue* parse_value_true(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonValue* parse_value_true(FILE* file, int* line_num)
 {
-    UNUSED(path);
     UNUSED(file);
     UNUSED(line_num);
-    UNUSED(error_count);
     return NULL;
 }
 
-static JsonValue* parse_value_false(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonValue* parse_value_false(FILE* file, int* line_num)
 {
-    UNUSED(path);
     UNUSED(file);
     UNUSED(line_num);
-    UNUSED(error_count);
     return NULL;
 }
 
-static JsonValue* parse_value_null(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonValue* parse_value_null(FILE* file, int* line_num)
 {
-    UNUSED(path);
     UNUSED(file);
     UNUSED(line_num);
-    UNUSED(error_count);
     return NULL;
 }
 
-static JsonValue* parse_value(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonValue* parse_value(FILE* file, int* line_num)
 {
     char c = peek_next_nonspace(file, line_num);
     if (c == '[')
-        return parse_value_array(path, file, line_num, error_count);
+        return parse_value_array(file, line_num);
     if (c == '{')
-        return parse_value_object(path, file, line_num, error_count);
+        return parse_value_object(file, line_num);
     if (c == '-' || isdigit(c))
-        return parse_value_number(path, file, line_num, error_count);
+        return parse_value_number(file, line_num);
     if (c == '"')
-        return parse_value_string(path, file, line_num, error_count);
+        return parse_value_string(file, line_num);
     if (c == 't')
-        return parse_value_true(path, file, line_num, error_count);
+        return parse_value_true(file, line_num);
     if (c == 'f')
-        return parse_value_false(path, file, line_num, error_count);
+        return parse_value_false(file, line_num);
     if (c == 'n')
-        return parse_value_null(path, file, line_num, error_count);
-    print_error(path, line_num, "Invalid value");
+        return parse_value_null(file, line_num);
+    print_error(line_num, "Invalid value");
     return NULL;
 }
 
-static JsonMember* parse_member(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonMember* parse_member(FILE* file, int* line_num)
 {
     char c;
     
-    const char* key = get_next_string(path, file, line_num);
+    const char* key = get_next_string(file, line_num);
     if (key == NULL) {
-        print_error(path, line_num, "Error reading key");
+        print_error(line_num, "Error reading key");
         return NULL;
     }
 
     c = get_next_nonspace(file, line_num);
     if (c != ':') {
-        print_error(path, line_num, "Missing colon");
+        print_error(line_num, "Missing colon");
         return NULL;
     }
 
     JsonValue* value;
-    value = parse_value(path, file, line_num, error_count);
+    value = parse_value(file, line_num);
     if (value == NULL) {
-        print_error(path, line_num, "Error reading value");
+        print_error(line_num, "Error reading value");
         return NULL;
     }
 
@@ -260,22 +252,19 @@ static JsonMember* parse_member(const char* path, FILE* file, int* line_num, int
     member->key = key;
     member->value = value;
 
-    UNUSED(path);
-    UNUSED(error_count);
-
     return member;
 }
 
-static JsonMember** parse_members(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonMember** parse_members(FILE* file, int* line_num)
 {
     char c;
     int num_members = 0;
     JsonMember** members = NULL;
     JsonMember* member;
 
-    member = parse_member(path, file, line_num, error_count);
+    member = parse_member(file, line_num);
     if (member == NULL) {
-        print_error(path, line_num, "Error parsing member");
+        print_error(line_num, "Error parsing member");
         return NULL;
     }
 
@@ -287,13 +276,13 @@ static JsonMember** parse_members(const char* path, FILE* file, int* line_num, i
     do {
         c = get_next_nonspace(file, line_num);
         if (c != ',') {
-            print_error(path, line_num, "Missing comma between members");
+            print_error(line_num, "Missing comma between members");
             json_members_destroy(members);
             return NULL;
         }
-        member = parse_member(path, file, line_num, error_count);
+        member = parse_member(file, line_num);
         if (member == NULL) {
-            print_error(path, line_num, "Error parsing member");
+            print_error(line_num, "Error parsing member");
             json_members_destroy(members);
             return NULL;
         }
@@ -305,21 +294,21 @@ static JsonMember** parse_members(const char* path, FILE* file, int* line_num, i
     return members;
 }
 
-static JsonObject* parse_object(const char* path, FILE* file, int* line_num, int* error_count)
+static JsonObject* parse_object(FILE* file, int* line_num)
 {
     JsonObject* object;
     char c;
 
     c = get_next_nonspace(file, line_num);
     if (c == EOF) {
-        print_error(path, line_num, "Expected '{'");
+        print_error(line_num, "Expected '{'");
         return NULL;
     }
     if (c != '{') {
         if (c == '}')
-            print_error(path, line_num, "Missing '{'");
+            print_error(line_num, "Missing '{'");
         else
-            print_error(path, line_num, "Unexpected character before '{'");
+            print_error(line_num, "Unexpected character before '{'");
         return NULL;
     }
 
@@ -334,10 +323,10 @@ static JsonObject* parse_object(const char* path, FILE* file, int* line_num, int
         return object;
     }
 
-    JsonMember** members = parse_members(path, file, line_num, error_count);
+    JsonMember** members = parse_members(file, line_num);
 
     if (members == NULL) {
-        print_error(path, line_num, "Error parsing objects");
+        print_error(line_num, "Error parsing objects");
         return NULL;
     }
 
@@ -348,7 +337,7 @@ static JsonObject* parse_object(const char* path, FILE* file, int* line_num, int
     c = get_next_nonspace(file, line_num);
 
     if (c != '}') {
-        print_error(path, line_num, "Expected '}'");
+        print_error(line_num, "Expected '}'");
         json_object_destroy(object);
         return NULL;
     }
@@ -366,10 +355,9 @@ JsonObject* json_read(const char* path)
         return NULL;
     }
     int line_num = 1;
-    int error_count = 0;
-    JsonObject* object = parse_object(path, file, &line_num, &error_count);
+    JsonObject* object = parse_object(file, &line_num);
     if (object != NULL && get_next_nonspace(file, &line_num) != EOF) {
-        print_error(path, &line_num, "Excess characters");
+        print_error(&line_num, "Excess characters");
         json_object_destroy(object);
         object = NULL;
     }
@@ -439,52 +427,27 @@ void json_object_destroy(JsonObject* object)
     UNUSED(object);
 }
 
-const char* negatives[] = {
-    "missing_file",
-    "missing_opening_brackets",
-    "unrecognized_char",
-    "missing_closing_brackets",
-    "characters_after_root",
-    "missing_endquote"
-};
-
-const char* positives[] = {
-    "test1",
-    "test2",
-    "test3",
-};
+void test(const char* path)
+{
+    char str[512];
+    DIR* negatives = opendir(path);
+    assert(negatives != NULL);
+    struct dirent* entry;
+    while ((entry = readdir(negatives)) != NULL) {
+        if (!strcmp(entry->d_name, "."))
+            continue;
+        if (!strcmp(entry->d_name, ".."))
+            continue;
+        sprintf(str, "%s/%s", path, entry->d_name);
+        JsonObject* object = json_read(str);
+        json_object_destroy(object);
+        puts("-------------------------");
+    }
+}
 
 int main(void)
 {
-    JsonObject* obj;
-    char str[256];
-    int total = 0;
-    int passed = 0;
-    for (int i = 0; i < (int)(sizeof(negatives)/sizeof(char*)); i++) {
-        sprintf(str, "tests/%s.json", negatives[i]);
-        obj = json_read(str);
-        if (obj == NULL)
-            passed++;
-        total++;
-        json_object_destroy(obj);
-        puts("----------------------------");
-    }
-    puts("");
-    printf("Passed %d / %d\n", passed, total);
-    puts("");
-    puts("===========================");
-    puts("");
-    total = passed = 0;
-    for (int i = 0; i < (int)(sizeof(positives)/sizeof(char*)); i++) {
-        sprintf(str, "tests/%s.json", positives[i]);
-        obj = json_read(str);
-        if (obj != NULL)
-            passed++;
-        total++;
-        json_object_destroy(obj);
-        puts("----------------------------");
-    }
-    puts("");
-    printf("Passed %d / %d\n", passed, total);
+    test("negatives");
+    test("positives");
     return 0;
 }
